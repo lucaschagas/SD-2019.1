6cfc3cb350fa597c6a821ed28ef14f18a4fb3411
Lucas Chagas <lucascbguimaraes@poli.ufrj.br>
2019-06-18 04:07:07 -0300

    TP3 - Versão 6

--- Trabalho 3/bully_algorithm.go
+++ Trabalho 3/bully_algorithm.go
@@ -19,14 +19,16 @@ const (
 
 var comando_buffer bytes.Buffer
 
-var mensagens_enviadas[6] int 
-var mensagens_recebidas[6] int
+var mensagens_enviadas[5] int 
+var mensagens_recebidas[5] int
 
 var lider_atual int
 var lider_status int
 var process_state int
-var election_status_start int
-var election_status_receive int
+var process_id int
+var process_rank int
+var ongoing_election int
+var teste_ok int
 
 var comando_buffer_lock = &sync.Mutex{}
 var mensagens_enviadas_lock = &sync.Mutex{}
@@ -34,6 +36,8 @@ var mensagens_recebidas_lock = &sync.Mutex{}
 var lider_atual_lock = &sync.Mutex{}
 var lider_status_lock = &sync.Mutex{}
 var process_state_lock = &sync.Mutex{}
+var ongoing_election_lock = &sync.Mutex{}
+var ok_lock = &sync.Mutex{}
 
 func msgHandler(src *net.UDPAddr, n int, b []byte) {
 	s := string(b)
@@ -66,27 +70,87 @@ func Listen(address string, handler func(*net.UDPAddr, int, []byte), port int) {
 	}
 }
 
+func start_election() {
+	ongoing_election_lock.Lock()
+	ongoing_election = 1
+	ongoing_election_lock.Unlock()
+
+	ok_lock.Lock()
+	teste_ok = 0
+	ok_lock.Unlock()
+
+	send_message("0" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", multicastPort)
+	update_sent_messages(0)
+	time.Sleep(6 * time.Second)
+
+	ok_lock.Lock()
+	if teste_ok == 0 {
+		ok_lock.Unlock()
+		send_message("2" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", multicastPort)
+		update_sent_messages(2)
+	} else {
+		ok_lock.Unlock()
+	}
+
+	ongoing_election_lock.Lock()
+	ongoing_election = 0
+	ongoing_election_lock.Unlock()
+}
+
+func check_leader_alive() (answer int) {
+	lider_status_lock.Lock()
+	lider_status = 0
+	lider_status_lock.Unlock()
+
+	lider_atual_lock.Lock()
+	send_message("3" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", lider_atual)
+	lider_atual_lock.Unlock()
+
+	update_sent_messages(3)
+
+	time.Sleep(5 * time.Second)
+	lider_status_lock.Lock()
+	if lider_status == 1 {
+		lider_status_lock.Unlock()
+		return 1
+	} else {
+		lider_status_lock.Unlock()
+		return 0
+	}
+}
+
+func update_received_messages(i int) {
+	mensagens_recebidas_lock.Lock()
+	mensagens_recebidas[i] += 1
+	mensagens_recebidas_lock.Unlock()
+}
+
+func update_sent_messages(i int) {
+	mensagens_enviadas_lock.Lock()
+	mensagens_enviadas[i] += 1
+	mensagens_enviadas_lock.Unlock()
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	process_id := rand.Intn(9997)+1
-	process_rank := process_id
+	process_id = rand.Intn(7998)+1125
+	process_rank = process_id
 
-	fmt.Println("")
-	fmt.Println(process_id)
-	fmt.Println("")
-
-	// Implementar semáforo para estados do líder e do processo
 	process_state = 1 //0: Falha; 1: Funcionando
 	lider_status = 1 //0: Morto; 1: Vivo
+	ongoing_election = 0 //0: Sem eleição em execução; 1: Eleição em execução
+	teste_ok = 0 //0: Nenhum OK recebido durante eleição; 1: Ao menos 1 OK recebido durante eleição
+
+	fmt.Println("ID do processo:", process_id)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
 
 	// Interface com o usuário
 	go func() {
-
 		var comando string
+
 		for true {
 
 		  	fmt.Println("Digite o comando desejado; para mais informações, digite \"help\".")
@@ -100,6 +164,7 @@ func main() {
 			  		fmt.Println("recover - Recupera um processo que esteja emulando falha.")
 			  		fmt.Println("stats - Imprime o atual líder do sistema, e a quantidade de mensagens enviadas e recebidas de cada tipo.")
 			  		fmt.Println("alive - Verifica se o líder atual está vivo; caso não esteja, avisa todos os processos que o líder está morto.")
+			  		fmt.Println("clear - Reseta a quantidade de mensagens enviadas e recebidas imprimida pelo comando stats.")
 
 			  	case "election":
 
@@ -115,22 +180,14 @@ func main() {
 			  		if process_state == 1 {
 			  			process_state_lock.Unlock()
 
-				  		election_status_start = 0 //0: Não recebeu OK; 1: Recebeu OK
-				  		send_message("0" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", multicastPort)
-				  		mensagens_enviadas_lock.Lock()
-				  		mensagens_enviadas[0] += 1
-				  		mensagens_enviadas_lock.Unlock()
-				  		time.Sleep(time.Duration(4) * time.Second)
+			  			ongoing_election_lock.Lock()
+						if ongoing_election == 0 {
+							ongoing_election_lock.Unlock()
+							go start_election()
+						} else {
+							ongoing_election_lock.Unlock()
+						}
 
-				  		if election_status_start == 0 {
-					  		send_message("2" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", multicastPort)
-					  		mensagens_enviadas_lock.Lock()
-					  		mensagens_enviadas[2] += 1
-					  		mensagens_enviadas_lock.Unlock()
-					  		fmt.Println("Comando executado com sucesso.")
-					  	} else {
-					  		fmt.Println("Existe processo ativo com rank maior·")
-					  	}
 					} else {
 			  			process_state_lock.Unlock()		
 			  		}
@@ -174,13 +231,17 @@ func main() {
 			  		}
 
 			  	case "stats":
+			  		lider_atual_lock.Lock()
+			  		fmt.Printf("Líder atual do sistema: %d\n", lider_atual)
+			  		lider_atual_lock.Unlock()
 			  		mensagens_enviadas_lock.Lock()
-			  		fmt.Printf("Mensagens enviadas- ELEICAO: %d | OK: %d | LIDER: %d | VIVO: %d | VIVO_OK: %d | LIDER_MORTO: %d\n", mensagens_enviadas[0], mensagens_enviadas[1], mensagens_enviadas[2], mensagens_enviadas[3], mensagens_enviadas[4], mensagens_enviadas[5])
+			  		fmt.Printf("Mensagens enviadas- ELEICAO: %d | OK: %d | LIDER: %d | VIVO: %d | VIVO_OK: %d\n",
+			  			mensagens_enviadas[0], mensagens_enviadas[1], mensagens_enviadas[2], mensagens_enviadas[3], mensagens_enviadas[4])
 			  		mensagens_enviadas_lock.Unlock()
 			  		mensagens_recebidas_lock.Lock()
-			  		fmt.Printf("Mensagens recebidas- ELEICAO: %d | OK: %d | LIDER: %d | VIVO: %d | VIVO_OK: %d | LIDER_MORTO: %d\n", mensagens_recebidas[0], mensagens_recebidas[1], mensagens_recebidas[2], mensagens_recebidas[3], mensagens_recebidas[4], mensagens_recebidas[5])
+			  		fmt.Printf("Mensagens recebidas- ELEICAO: %d | OK: %d | LIDER: %d | VIVO: %d | VIVO_OK: %d\n",
+			  			mensagens_recebidas[0], mensagens_recebidas[1], mensagens_recebidas[2], mensagens_recebidas[3], mensagens_recebidas[4])
 			  		mensagens_recebidas_lock.Unlock()
-			  		fmt.Println("Comando executado com sucesso.")
 
 			  	case "alive":
 
@@ -189,47 +250,26 @@ func main() {
 			  			process_state_lock.Unlock()
 			  			fmt.Println("Processo está simulando uma falha, e não pode enviar mensagens.")
 			  		} else {
-			  			process_state_lock.Unlock()		
-			  		}
-
-			  		process_state_lock.Lock()
-			  		if process_state == 1 {
 			  			process_state_lock.Unlock()
+			  			manual_check := check_leader_alive()
 
-			  			lider_atual_lock.Lock()
-				  		send_message("3" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", lider_atual)
-				  		lider_atual_lock.Unlock()
-				  		mensagens_enviadas_lock.Lock()
-				  		mensagens_enviadas[3] += 1
-				  		mensagens_enviadas_lock.Unlock()
-				  		lider_status_lock.Lock()
-				  		lider_status = 0
-				  		lider_status_lock.Unlock()
-
-				  		time.Sleep(time.Duration(3) * time.Second)
-				  		lider_status_lock.Lock()
-				  		if lider_status == 1 {
-				  			lider_status_lock.Unlock()
-				  			fmt.Println("Líder está vivo.")
-				  		} else {
-				  			lider_status_lock.Unlock()
-				  			time.Sleep(time.Duration(3) * time.Second)
-				  			lider_status_lock.Lock()
-				  			if lider_status == 1 {
-				  				lider_status_lock.Unlock()
-				  				fmt.Println("Líder está vivo.")
-				  			} else {
-				  				lider_status_lock.Unlock()
-				  				fmt.Println("Líder está morto.")
-				  				send_message("5" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", multicastPort)
-				  				mensagens_enviadas_lock.Lock()
-				  				mensagens_enviadas[5] += 1
-				  				mensagens_enviadas_lock.Unlock()
-				  			}
-				  		}
-				  	} else {
-			  			process_state_lock.Unlock()		
+			  			if manual_check == 1 {
+							fmt.Println("Líder está vivo.")
+			  			} else {
+			  				fmt.Println("Líder está morto.")
+			  			}		
 			  		}
+
+			  	case "clear":
+
+					mensagens_recebidas_lock.Lock()
+					mensagens_enviadas_lock.Lock()
+					for i:= 0; i < 5; i++ {
+						mensagens_enviadas[i] = 0
+						mensagens_recebidas[i] = 0
+					}
+					mensagens_recebidas_lock.Unlock()
+					mensagens_enviadas_lock.Unlock()
 		  	}
 	  	}
 	}()
@@ -252,7 +292,7 @@ func main() {
 			if comando_atual_string != "" {
 
 				string_split := strings.Split(comando_atual_string, "|")
-				time.Sleep(time.Duration(1) * time.Second)
+				time.Sleep(30 * time.Millisecond)
 				codigo, processo_string, valor := string_split[0], string_split[1], string_split[2]
 
 				valor_temp := strings.TrimSuffix(valor, ";")
@@ -260,14 +300,16 @@ func main() {
 				codigo_int, _ := strconv.Atoi(codigo)
 				processo_int, _ := strconv.Atoi(processo_string)
 
-				fmt.Println("Código:", codigo, "| Valor:", valor_int)
+				/*if processo_int == process_id && lider_atual != process_id {
+					process_id = rand.Intn(7998)+1125
+				}*/
+
+				//fmt.Println("Código:", codigo, "| Valor:", valor_int)
 
 				switch codigo_int {
 					case 0:
-						fmt.Println("Código ELEIÇÃO")
-						mensagens_recebidas_lock.Lock()
-						mensagens_recebidas[0] += 1
-						mensagens_recebidas_lock.Unlock()
+						//fmt.Println("Código ELEIÇÃO")
+						update_received_messages(0)
 
 						process_state_lock.Lock()
 						if process_state == 1 {
@@ -276,22 +318,14 @@ func main() {
 							if process_rank > valor_int {
 
 								send_message("1" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", processo_int)
-								mensagens_enviadas_lock.Lock()
-								mensagens_enviadas[1] += 1
-								mensagens_enviadas_lock.Unlock()
+								update_sent_messages(1)
 
-								election_status_receive = 0
-								send_message("0" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", multicastPort)
-								mensagens_enviadas_lock.Lock()
-								mensagens_enviadas[0] += 1
-								mensagens_enviadas_lock.Unlock()
-								time.Sleep(time.Duration(4) * time.Second)
-								
-								if election_status_receive == 0 {
-									send_message("2" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", multicastPort)
-									mensagens_enviadas_lock.Lock()
-									mensagens_enviadas[2] += 1
-									mensagens_enviadas_lock.Unlock()
+								ongoing_election_lock.Lock()
+								if ongoing_election == 0 {
+									ongoing_election_lock.Unlock()
+									go start_election()
+								} else {
+									ongoing_election_lock.Unlock()
 								}
 							}
 						} else {
@@ -299,136 +333,69 @@ func main() {
 			  			}
 
 					case 1:
-						fmt.Println("Código OK")
-						mensagens_recebidas_lock.Lock()
-						mensagens_recebidas[1] += 1
-						mensagens_recebidas_lock.Unlock()
+						//fmt.Println("Código OK")
+						update_received_messages(1)
 
-						election_status_start = 1
-						election_status_receive = 1
+						ok_lock.Lock()
+						teste_ok = 1
+						ok_lock.Unlock()
 
 					case 2:
-						fmt.Println("Código LÍDER")
-						mensagens_recebidas_lock.Lock()
-						mensagens_recebidas[2] += 1
-						mensagens_recebidas_lock.Unlock()
+						//fmt.Println("Código LÍDER")
+						update_received_messages(2)
 
 						lider_atual_lock.Lock()
-						lider_status_lock.Lock()
-						if lider_atual < valor_int && lider_status == 1 {
-							lider_atual = valor_int
-						}
-
-						if lider_status == 0 {
-							lider_atual = valor_int
-							lider_status = 1
-						}
-						lider_status_lock.Unlock()
+						lider_atual = valor_int
+						fmt.Println("Líder atual:", lider_atual)
 						lider_atual_lock.Unlock()
 
 					case 3:
-						fmt.Println("Código VIVO")
-						mensagens_recebidas_lock.Lock()
-						mensagens_recebidas[3] += 1
-						mensagens_recebidas_lock.Unlock()
+						//fmt.Println("Código VIVO")
+						update_received_messages(3)
 
 						process_state_lock.Lock()
 						if process_state == 1 {
 							process_state_lock.Unlock()
-							lider_atual_lock.Lock()
-							if process_rank == lider_atual {
-								lider_atual_lock.Unlock()
-								send_message("4" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", processo_int)
-								mensagens_enviadas_lock.Lock()
-								mensagens_enviadas[4] += 1
-								mensagens_enviadas_lock.Unlock()
-							} else {
-			  					lider_atual_lock.Unlock()		
-			  				}
+							send_message("4" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", processo_int)
+							update_sent_messages(4) 
 						} else {
 			  				process_state_lock.Unlock()		
 			  			}
 
 					case 4:
-						fmt.Println("Código VIVO_OK")
-						mensagens_recebidas_lock.Lock()
-						mensagens_recebidas[4] += 1
-						mensagens_recebidas_lock.Unlock()
+						//fmt.Println("Código VIVO_OK")
+						update_received_messages(4)
 
 						lider_status_lock.Lock()
 						lider_status = 1
 						lider_status_lock.Unlock()
-
-					case 5:
-						fmt.Println("Código DEAD_LÍDER")
-						mensagens_recebidas_lock.Lock()
-						mensagens_recebidas[5] += 1
-						mensagens_recebidas_lock.Unlock()
-						
-						lider_status_lock.Lock()
-						lider_status = 0
-						lider_status_lock.Unlock()
 				}
 			} else {
-				time.Sleep(time.Duration(10) * time.Millisecond)
+				time.Sleep(10 * time.Millisecond)
 			}
 		}
 	}()
 
-	// Detecção do líder
+	// Detecção automática do líder
 	go func() {
-
 		for true {
-			time.Sleep(time.Duration(40) * time.Second)
+			time.Sleep(400 * time.Second)
 
-			lider_atual_lock.Lock()
-			send_message("3" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", lider_atual)
-			lider_temp := lider_atual
-			lider_atual_lock.Unlock()
-			mensagens_enviadas_lock.Lock()
-			mensagens_enviadas[3] += 1
-			mensagens_enviadas_lock.Unlock()
-			lider_status_lock.Lock()
-			lider_status = 0
-			lider_status_lock.Unlock()
+			if process_state == 1 {
+				auto_check := check_leader_alive()
 
-			time.Sleep(time.Duration(7) * time.Second)
+				if auto_check == 0 {
+					ongoing_election_lock.Lock()
 
-			lider_status_lock.Lock()
-			if lider_status == 0 && lider_temp == lider_atual {
-				lider_status_lock.Unlock()
-				send_message("5" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", multicastPort)
-				mensagens_enviadas_lock.Lock()
-				mensagens_enviadas[5] += 1
-				mensagens_enviadas_lock.Unlock()
-				
-				election_status_receive = 0
-				send_message("0" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", multicastPort)
-				mensagens_enviadas_lock.Lock()
-				mensagens_enviadas[0] += 1
-				mensagens_enviadas_lock.Unlock()
-				time.Sleep(time.Duration(4) * time.Second)
-								
-				if election_status_receive == 0 {
-					send_message("2" + "|" + fmt.Sprint(process_id) + "|" + fmt.Sprint(process_rank) + ";", multicastPort)
-					mensagens_enviadas_lock.Lock()
-					mensagens_enviadas[2] += 1
-					mensagens_enviadas_lock.Unlock()
-				}				
-			} else {
-				lider_status_lock.Unlock()		
+					if ongoing_election == 0 {
+						ongoing_election_lock.Unlock()
+						go start_election()
+					} else {
+						ongoing_election_lock.Unlock()
+					}
+			  	}
 			}
 	  	}
-	}()
-
-	// Imprime o lider atual a cada 10 segundos
-	go func() {
-		for true {
-			time.Sleep(time.Duration(10) * time.Second)
-			fmt.Println("")
-			fmt.Println(lider_atual)
-			fmt.Println("")
-		}
 	}()
 
 	wg.Wait()
